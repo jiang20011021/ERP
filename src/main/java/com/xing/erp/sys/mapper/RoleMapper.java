1f7ced33fb050d1afa1e1545d7f3e38eefba4ff0
jiang20011021 <2277200858@qq.com>
2021-01-10 22:23:17 +0800

    jzw

--- src/main/java/com/xing/erp/sys/mapper/RoleMapper.java
+++ src/main/java/com/xing/erp/sys/mapper/RoleMapper.java
@@ -3,13 +3,13 @@ package com.xing.erp.sys.mapper;
 import com.xing.erp.sys.model.Role;
 
 public interface RoleMapper {
-    int deleteByPrimaryKey(Integer uuid);
+    int deleteByPrimaryKey(Long roleid);
 
     int insert(Role record);
 
     int insertSelective(Role record);
 
-    Role selectByPrimaryKey(Integer uuid);
+    Role selectByPrimaryKey(Long roleid);
 
     int updateByPrimaryKeySelective(Role record);
 
