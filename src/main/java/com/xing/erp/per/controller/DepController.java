564749eed849552b0affa1b05519ce2950482af8
Jun011006 <2558180246@qq.com>
2021-01-13 09:53:57 +0800

    cjj1.1

---
+++ src/main/java/com/xing/erp/per/controller/DepController.java
@@ -0,0 +1,15 @@
+package com.xing.erp.per.controller;
+
+
+import com.xing.erp.per.service.IDepService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Dep")
+public class DepController {
+    private IDepService depService;
+
+
+
+}
--- src/main/java/com/xing/erp/per/service/impl/DepServiceImpl.java
+++ src/main/java/com/xing/erp/per/service/impl/DepServiceImpl.java
@@ -1,36 +1,40 @@
 package com.xing.erp.per.service.impl;
 
+import com.xing.erp.per.mapper.DepMapper;
 import com.xing.erp.per.model.Dep;
 import com.xing.erp.per.service.IDepService;
 
 public class DepServiceImpl implements IDepService {
+
+    private DepMapper depMapper;
+
     @Override
     public int deleteByPrimaryKey(Integer uuid) {
-        return 0;
+        return depMapper.deleteByPrimaryKey(uuid);
     }
 
     @Override
     public int insert(Dep record) {
-        return 0;
+        return depMapper.insert(record);
     }
 
     @Override
     public int insertSelective(Dep record) {
-        return 0;
+        return depMapper.insertSelective(record);
     }
 
     @Override
     public Dep selectByPrimaryKey(Integer uuid) {
-        return null;
+        return depMapper.selectByPrimaryKey(uuid);
     }
 
     @Override
     public int updateByPrimaryKeySelective(Dep record) {
-        return 0;
+        return depMapper.updateByPrimaryKeySelective(record);
     }
 
     @Override
     public int updateByPrimaryKey(Dep record) {
-        return 0;
+        return depMapper.updateByPrimaryKey(record);
     }
 }
